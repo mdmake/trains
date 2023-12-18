cdba10c80be95c3f45c9b4e2aab69c97faba34a0
jonnyboooy <103967803+jonnyboooy@users.noreply.github.com>
2023-12-18 13:40:06 +0300

    Исправленная версия сцены
    Установка сцены теперь производится с помощью функционала объекта класса Map.

--- game/scene.py
+++ game/scene.py
@@ -1,6 +1,5 @@
 import pymunk
-from math import radians
-import yaml
+from map import Map
 
 
 class Scene:
@@ -10,26 +9,19 @@ class Scene:
 
     def __init__(self, filename, space):
         self.space = space
-        with open(filename, 'r') as f:
-            data = yaml.safe_load(f)
-
-            for k in data['circles'].keys():
-                data['circles'][k]['type'] = "circle"
-            for k in data['rectangles'].keys():
-                data['rectangles'][k]['type'] = "rectangle"
-
-            self.border = data['border']
-            self.objects = data['circles']
-            self.objects.update(data['rectangles'])
-            self.description = data['description']
-            self._complete = self.description['complete']
+        self.map = Map(filename)
 
     def set_scene(self):
-        count = len(self.border['coordinates'])
+        """
+        Установка карты.
+        """
+
+        count = len(self.map.border['coordinates'])
+
         for i in range(count):
             segment_shape_bottom = pymunk.Segment(
                 self.space.static_body,
-                self.border['coordinates'][i], self.border['coordinates'][(i + 1) % count],
+                self.map.border['coordinates'][i], self.map.border['coordinates'][(i + 1) % count],
                 2
             )
             segment_shape_bottom.id = i
@@ -38,19 +30,16 @@ class Scene:
             segment_shape_bottom.friction = 1.0
             self.space.add(segment_shape_bottom)
 
-        for k, object in self.objects.items():
-            if object['type'] == "circle":
-                circle_body = pymunk.Body(body_type=pymunk.Body.STATIC)
-                circle_body.position = object['center']
-                circle_shape = pymunk.Circle(circle_body, object['radius'])
-                circle_shape.body = circle_body
-                self.space.add(circle_shape, circle_body)
-            elif object['type'] == "rectangle":
-                box_body = pymunk.Body(body_type=pymunk.Body.STATIC)
-                box_body.position = 0, 0
-                box_shape = pymunk.Poly(box_body, object['coordinates'])
-                box_shape.body = box_body
-                self.space.add(box_shape, box_body)
-            else:
-                error_message = f"Unknown type: {object['type']}"
-                raise KeyError(error_message)
+        for circle in self.map.circles:
+            circle_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+            circle_body.position = self.map.circles[circle]['center']
+            circle_shape = pymunk.Circle(circle_body, self.map.circles[circle]['radius'])
+            circle_shape.body = circle_body
+            self.space.add(circle_shape, circle_body)
+
+        for rectangle in self.map.rectangles:
+            box_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+            box_body.position = 0, 0
+            box_shape = pymunk.Poly(box_body, self.map.rectangles[rectangle]['coordinates'])
+            box_shape.body = box_body
+            self.space.add(box_shape, box_body)
