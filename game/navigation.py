7dfdf2f6d748b87158d542650d77802fdea1f845
Mikhail Prokhorov <mmake.md@gmail.com>
2023-12-25 12:06:16 +0200

    Переписал навигацию и тесты к ней.

--- game/navigation.py
+++ game/navigation.py
@@ -1,52 +1,93 @@
 from math import sin, cos, radians
-from typing import Tuple
-from .mathfunction import sign
+from typing import Callable
+
 import yaml
 
+from game.exceptions import ConfigError
+from game.mathfunction import sign
+from game.trainsystem import TrainSystem
 
-class NavigationSystem:
 
-    def __init__(self, x, y, angle, config):
+class NavigationSystem(TrainSystem):
+    """
+    Класс навигации выдает всем остальным подсистемам координаты и курс.
+    Считывает настройки из конфига (`_load_config`)или словаря (`_unpack_config`)
+
+    Класс получает на вход управление в виде вектора желаемого передвижения за этот такт (`v`, `alpha`) и
+    выдает новое местоположение основываясь на наличии или отсутствии препятствий и
+    ограничений на скорость и угловую скорость
+    """
+
+    def __init__(
+            self, x: float | int, y: float | int, alpha: float | int, config: str | dict
+    ):
         self.x = x
         self.y = y
-        self.angle = angle
+        self.alpha = alpha
 
-        self.method = None
-        self.method_kwargs = None
+        self._new_v = 0.0  # управление по v
+        self._new_alpha = 0.0  # управление по alpha
+
+        self.method = None  # метод для обнаружения коллизий
+        self.method_kwargs = None  # аргументы для этого метода
 
         if isinstance(config, dict):
-            self.config = self._unpack_config(config)
+            self._unpack_config(config)
         else:
-            self.config = self._load_config(config)
+            self._load_config(config)
 
-        self.v_max = self.config['v_max']
-        self.max_angle_speed = self.config['max_angle_speed']
+        self.v_max = self.config["v_max"]
+        self.max_angle_speed = self.config["max_angle_speed"]
         self.collision = False
 
-    def _unpack_config(self, data):
-        config = data.copy()
-        config["max_angle_speed"] = config["max_angle_speed"]
-        config["v_max"] = config["cone_opening_angle"]
-        return config
+    def _unpack_config(self, config: dict):
+        """
+        Чтение конфигурации из словаря. Все угловые величины должны быть в радианах!
 
-    def _load_config(self, filename):
-        config = {}
-        with open(filename, 'r') as f:
-            data = yaml.safe_load(f)['train']['tth']
+        :param config: Словарь с конфигурацией.
+        """
+        self.config = config.copy()
 
-            config["max_angle_speed"] = radians(data["max_angle_speed"])
-            config["v_max"] = radians(data["cone_opening_angle"])
+        for item in ['max_angle_speed', "v_max"]:
+            if item not in self.config:
+                raise ConfigError(f"{item} not in config")
 
-        return config
+    def _load_config(self, filename: str):
+        """
+        Чтение конфигурации из файла. Все угловые величины должны быть в градусах!
 
-    def set_measurement_method(self, method, **kwargs):
+        :param filename: Путь к файлу конфигурации
+        :return:
+        """
+        self.config = {}
+        with open(filename, "r") as f:
+            data = yaml.safe_load(f)["train"]["tth"]
+
+        for item in ['max_angle_speed', "v_max"]:
+            if item not in self.config:
+                raise ConfigError(f"{item} not in config")
+
+        self.config["max_angle_speed"] = radians(data["max_angle_speed"])
+
+    def set_measurement_method(self, method: Callable, **kwargs):
+        """
+        Ссылка на метод для поиска коллизий.
+
+        :param method: Метод для измерения дальности;
+        :param kwargs: Аргументы этого метода.
+        """
         self.method = method
         self.method_kwargs = kwargs
 
-    def step_restriction(self, alpha, v) -> tuple[tuple[float, float], bool]:
+    def step_restriction(self) -> tuple[float, float, float, bool]:
+        """
+        Проверка возможности перемещения на новою позицию
+        """
 
-        v = min(v, self.v_max)
-        alpha = sign(alpha) * min(alpha, self.max_angle_speed)
+        v = min(self._new_v, self.v_max)
+        alpha = self.alpha + sign(self._new_alpha) * min(
+            abs(self._new_alpha - self.alpha), self.max_angle_speed
+        )
 
         x = self.x + v * cos(alpha)
         y = self.y + v * sin(alpha)
@@ -54,9 +95,25 @@ class NavigationSystem:
         result = self.method((self.x, self.y), (x, y), **self.method_kwargs)
         if result:
             x, y = result
-            return (x, y), False
-        return (x, y), True
+            return x, y, alpha, True
+        return x, y, alpha, False
 
-    def step(self, alpha, v):
-        (x, y), self.collision = self.step_restriction(alpha, v)
-        return x, y
+    def receive(self, query: dict):
+        self._new_alpha = query["alpha"]
+        self._new_v = query["v"]
+
+    def step(self):
+        self.x, self.y, self.alpha, self.collision = self.step_restriction()
+        self._new_v = 0.0
+        self._new_alpha = 0.0
+
+    def send(self) -> dict:
+        return {
+            "x": self.x,
+            "y": self.y,
+            "alpha": self.alpha,
+            "collision": self.collision,
+        }
+
+    def update_navigation(self, x: float | int, y: float | int, alpha: float | int):
+        pass
---
+++ tests/test_navigation.py
@@ -0,0 +1,102 @@
+from math import radians, cos, sin
+
+from game.exceptions import ConfigError
+from game.mathfunction import sign
+from game.navigation import NavigationSystem
+
+EPS = 1e-5
+
+
+def test_creation():
+    config = {
+        "v_max": 20,  # максимальная скорость
+        "max_angle_speed": 5,  # максимальная угловая скорость
+    }
+    navigation = NavigationSystem(0, 0, 0, config)
+
+
+def test_config_invalid():
+    config = {}
+
+    try:
+        NavigationSystem(0, 0, 0, config)
+        return False
+    except ConfigError:
+        pass
+
+    config = {
+        "v_max": 20,
+    }
+    try:
+        NavigationSystem(0, 0, 0, config)
+        return False
+    except ConfigError:
+        pass
+
+    config = {
+        "max_angle_speed": 5,  # максимальная угловая скорость
+    }
+    try:
+        NavigationSystem(0, 0, 0, config)
+        return False
+    except ConfigError:
+        pass
+
+    return True
+
+
+def test_step_zero_without_collision():
+    config = {
+        "v_max": 10,  # максимальная скорость
+        "max_angle_speed": radians(10),  # максимальная угловая скорость
+    }
+
+    methods = [lambda crd0, crd1, **kwargs: None, lambda crd0, crd1, **kwargs: crd1]
+    responses = [False, True]
+
+    for method, responce in zip(methods, responses):
+        navigation = NavigationSystem(0, 0, 0, config)
+        navigation.set_measurement_method(method)
+
+        navigation.receive({"v": 0, 'alpha': radians(0)})
+        navigation.step()
+        state = navigation.send()
+
+        assert abs(state['x'] - 0.0) < EPS
+        assert abs(state['y'] - 0.0) < EPS
+        assert abs(state['alpha'] - 0.0) < EPS
+        assert state['collision'] is responce
+
+
+def test_movements_math():
+    configs = [
+        {"v_max": 10, "max_angle_speed": radians(10), },
+        {"v_max": 1000, "max_angle_speed": radians(360), },
+    ]
+
+    # нет коллизии
+
+    methods = [lambda crd0, crd1, **kwargs: None, lambda crd0, crd1, **kwargs: crd1]
+    responces = [False, True]
+    velosites = [0, 5, 10, 15]
+    alphas = [0, radians(5), radians(-5), radians(-90), radians(90), radians(180)]
+    x0, y0, alpha0 = 0, 0, 0
+
+    for config in configs:
+        for method, responce in zip(methods, responces):
+            for velocity in velosites:
+                for alpha in alphas:
+                    navigation = NavigationSystem(x=x0, y=y0, alpha=alpha0, config=config)
+                    navigation.set_measurement_method(method)
+
+                    navigation.receive({"v": velocity, 'alpha': alpha})
+                    navigation.step()
+                    state = navigation.send()
+
+                    correct_alpha = sign(state['alpha']) * min(abs(alpha - alpha0), config["max_angle_speed"])
+                    correct_x = min(velocity, config["v_max"]) * cos(correct_alpha)
+                    correct_y = min(velocity, config["v_max"]) * sin(correct_alpha)
+                    assert abs(state['alpha'] - correct_alpha) < EPS
+                    assert abs(state['x'] - correct_x) < EPS
+                    assert abs(state['y'] - correct_y) < EPS
+                    assert state['collision'] is responce
