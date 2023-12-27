d2e47b64c167280d8863a2c510a4cb1aae1d068c
Mikhail Prokhorov <mmake.md@gmail.com>
2023-12-27 20:46:17 +0200

    Зафиксировал состояние для диспетчера

--- game/dispatcher.py
+++ game/dispatcher.py
@@ -1,38 +1,94 @@
+from math import radians
+
 import pymunk
+
+from game.navigation import NavigationSystem
+from game.sightingsystem import Laser
+from game.sightingsystem import Locator
 from game.train import Train
-from math import sqrt, radians
-from game.locator import Locator
-from collections import namedtuple
+
+place = [5, 15]
+
+full_train_config = {
+    "tth": {"v_max": 20, "max_angle_speed": 5, },
+    "private": {"place": place}
+}
+
+full_laser_config = {
+    "min_range": 0,
+    "max_range": 100,
+    "max_angle_speed": radians(5),
+    "cone_opening_angle": radians(150),
+    "zero": radians(10),
+    "place": place,
+    "fire_power": 100,
+    "fire_time_limit": 5,
+    "max_angle_speed_tracking": radians(4),
+}
+
+full_locator_config = {
+    "min_range": 5,
+    "max_range": 40,
+    "max_angle_speed": radians(2),
+    "cone_opening_angle": radians(120),
+    "zero": radians(0),
+    "place": place,
+    "ray_count": 11,
+    "ray_step": 6,
+}
 
 
-class UpdatedTrain(Train):
+class TPlayer:
+    def __init__(self, method, method_kwargs):
+        self.navigation = NavigationSystem(x=0, y=0, alpha=0, config=full_train_config['tth'])
+        self.navigation.set_measurement_method(method, **method_kwargs)
 
-    def __init__(self,
-                 x0: float,
-                 y0: float,
-                 alpha0: float,
-                 v_max: float,
-                 locator: Locator,
-                 name: str = "Unknown Train",
-                 color: tuple[int] = (0, 0, 255)
-                 ):
-        super().__init__(x0, y0, alpha0, v_max, locator)
-        self.name = name
-        self.color = color
-        self.auto = False
-        self.v = 0
+        self.laser = Laser("test_laser", full_laser_config)
+        self.laser.set_measurement_method(method, **method_kwargs)
 
+        self.locator = Locator("test_locator", full_locator_config)
+        self.locator.set_measurement_method(method, **method_kwargs)
 
-class Dispatcher:
+        self.train = Train("test_locator", full_train_config)
 
-    def __init__(self):
-        self.players = []
+        self.to_laser = {}
+        self.to_locator = {}
+        self.to_train = {}
+        self.to_navigation = {"v": 0, 'alpha': radians(0)}
 
-    def append(self, player=None, player_config=None):
-        pass
+        self.from_laser = {}
+        self.from_locator = {}
+        self.from_train = {}
+        self.from_navigation = {}
 
     def step(self):
-        pass
+        self.navigation.receive(self.to_navigation)
+        self.navigation.step()
+        self.from_navigation = self.navigation.send()
+
+        self.from_navigation.pop("collision")
+
+        self.laser.update_navigation(**self.from_navigation)
+        self.laser.receive(self.to_laser)
+        self.laser.step()
+        self.from_laser = self.laser.send()
+
+        self.locator.update_navigation(**self.from_navigation)
+        self.locator.receive(self.to_locator)
+        self.locator.step()
+        self.from_locator = self.locator.send()
+
+        self.to_train['laser'] = self.from_laser
+        self.to_train['locator'] = self.from_locator
+
+        self.train.update_navigation(**self.from_navigation)
+        self.train.receive(self.to_train)
+        self.train.step()
+        self.from_train = self.train.send()
+
+        self.to_navigation = self.from_train['navigation']
+        self.to_locator = self.from_train['locator']
+        self.to_laser = self.from_train['laser']
 
 
 class Player:
@@ -44,111 +100,42 @@ class Player:
                  name: str = "Unknown Train",
                  color: tuple[int] = (0, 0, 255)
                  ):
-        self.position = position
-        self.angle = angle
         self.space = space
-        self.ray_length = 100
-        self.blind_zone = 5
-        self.collision = None
-        self.locator = Locator(self.ray_length, self.blind_zone)
+
         self.v_max = 10
         self.name = name
-        self.train = UpdatedTrain(
-            position[0],
-            position[1],
-            angle,
-            self.v_max,
-            self.locator,
-            self.name,
-            color)
-
+        self.color = color
         self.create_shapes()
-        self.set_touchpoint_invisible()
         self.map = set()
 
-    def create_shapes(self):
-        ray_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
-        ray_shape = pymunk.Segment(ray_body, (0, self.blind_zone), (0, self.ray_length), 1)
-        ray_body.position = self.position
-        ray_body.angle = -self.angle
-        ray_shape.color = (217, 34, 28, 255)
-        ray_shape.sensor = True
-        self.ray_body = ray_body
-        self.space.add(self.ray_body, ray_shape)
+        method = self.space.segment_query_first
+        method_kwargs = {'radius': 0.1, 'shape_filter': pymunk.ShapeFilter()}
+        self.train = TPlayer(method, method_kwargs)
 
+    def create_shapes(self):
         self.train_shape = pymunk.Poly(None, ((0.0, 0.0), (-15.0, -50.0), (15.0, -50.0)))
         train_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
         self.train_shape.body = train_body
-        train_body.angle = self.angle
-        train_body.position = self.position
+        train_body.angle = self.train.navigation.alpha
+        train_body.position = self.train.navigation.x, self.train.navigation.y
         train_body.sensor = False
 
         # train_shape.sensor = True
-        self.train_shape.color = (*self.train.color, 255)
+        self.train_shape.color = (*self.color, 255)
         self.train_body = train_body
         self.space.add(self.train_body, self.train_shape)
 
-        end_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
-        end_shape = pymunk.Circle(end_body, 5)
-        end_shape.color = (0, 0, 0, 0)
-        end_body.position = self.position
-        end_shape.sensor = True
-        self.end_body = end_body
-        self.end_shape = end_shape
-        self.space.add(self.end_body, self.end_shape)
-
-    def set_touchpoint_visible(self):
-        # self.end_shape.color = (255, 0, 0, 255)
-        pass
-
-    def set_touchpoint_invisible(self):
-        # self.end_shape.color = (0, 0, 0, 0)
-        pass
-
-    def make_query(self, query):
-
-        # self._query = LocatorQuery((x, y, alpha), begin_point, end_point)
-        if query:
-            return self.space.segment_query_first(query[1], query[2], 0.1, pymunk.ShapeFilter())
-
-    def update(self) -> dict[str, list]:
+    def step(self) -> dict[str, list]:
         # запрашиваем локатор
-        collision = self.make_query(self.locator.query)
 
-        if collision and collision.shape is not self.train_shape:
-            # self.set_touchpoint_visible()
-            # self.end_body.position = collision.pointqqq
-            # print(f"{self.locator.query} ==> {collision.point}")
+        self.train.step()
 
-            point0 = self.locator.query[0][0:2]
-            point1 = collision.point
-
-            distance = sqrt(
-                (point0[0] - point1[0]) ** 2 +
-                (point0[1] - point1[1]) ** 2
-            )
-        else:
-            print(f"{self.locator.query} ==> None")
-            self.set_touchpoint_invisible()
-            distance = None
-
-        self.locator._set_distance(distance)
-
-        # запускаем бот
-        self.train.update(*self.position)
-        self.train.processing()
-        info = self.train.info()
-
-        # меняем позицию на экране
-        self.position = info['params'][0], info['params'][1]
-        self.angle = info['params'][3]
-        self.train_body.angle = self.angle - radians(90)
-        self.train_body.position = self.position
+        # self.
 
         data = {
-            "points": info['maps']['points'],
-            "lines": info['maps']['lines'],
-            "circles": info['maps']['circles'],
+            "points": [],
+            "lines": [],
+            "circles": [],
         }
 
         return data
